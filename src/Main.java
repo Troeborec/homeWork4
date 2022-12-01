bb87c0dced7bee8681576d5bb5c653e7c3dc51c9
Troeborec <defied_jeff@mail.ru>
2022-12-02 01:42:21 +1200

    Задача 3

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        float a = 27.12f;
-        long b = 987678965549l;
-        float c = 2.786f;
-        short d = 569;
-        short e = -159;
-        int f = 27897;
-        byte g = 67;
+    byte teacher1 = 23;
+    byte teacher2 = 27;
+    byte teacher3 = 30;
+    short peaper = 480;
+    int allStudents = teacher1 + teacher2 + teacher3;
+    int everyStudents = peaper / allStudents;
+        System.out.println("На каждого ученика рассчитано " + everyStudents + " листов бумаги");
     }
 }

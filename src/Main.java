0ae844813e746a557422cab7334634564f6757f7
Troeborec <defied_jeff@mail.ru>
2022-12-02 01:22:07 +1200

    Задача 2

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-        byte a = 1;
-        short b = 2;
-        int c = 3;
-        long d = 4;
-        float A = 1.1f;
-        double B = 2.2;
-        System.out.println("Значение переменной byte " + "с типом целочисленных" + " равно " + a);
-        System.out.println("Значение переменной short " + " с типом целочиленных" + " равно " + b);
-        System.out.println("Значение переменной int " + " с типом целочисленных" + " равно " + c);
-        System.out.println("Значение переменной long " + " с типом целочисленных" + " равно " + d);
-        System.out.println("Значение переменной float" + " с типом плавающей точки" + " равно " + A);
-        System.out.println("Значение переменной float" + " с типом плавающей точки" + " равно " + B);
+        float a = 27.12f;
+        long two = 987678965549l;
+        float three = 2.786f;
+        short four = 569;
+        short five = -159;
+        int six = 27897;
+        byte seven = 67;
+        System.out.println(a);
     }
 }
